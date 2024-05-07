57fca7170666db7b2891121fc44d9e98e4dadb4d
ClaudioPachecoCP <clapache@gmail.com>
2024-05-07 18:43:53 -0300

    Tercer commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mi_paquete',
+    version='1.0',
+    packages=find_packages(),
+    description='Esto es un paquete distribuible de Python',
+    author='Claudio Pacheco',
+    author_email='clapache@gmail.com',
+)
